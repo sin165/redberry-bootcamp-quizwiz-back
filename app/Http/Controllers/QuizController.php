f947f3f8b46a24ff25b44d6bbfd981c9a4ca56bf
Tamar Sinauridze <sin165165@gmail.com>
2024-05-07 00:55:37 +0400

    feat: add quiz play functionality

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\TakeQuizRequest;
 use App\Http\Resources\QuizResource;
 use App\Models\Quiz;
+use App\Models\Result;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
@@ -26,5 +29,46 @@ class QuizController extends Controller
 	{
 		$quiz->load('questions.answers');
 		return new QuizResource($quiz);
+	}
+
+	public function take(TakeQuizRequest $request, Quiz $quiz): JsonResponse
+	{
+		if (auth()->id() && $quiz->results->firstWhere('user_id', auth()->id())) {
+			return response()->json(['message' => 'You have already taken this quiz'], 403);
+		}
+
+		['time' => $time, 'answers' => $answers] = $request->validated();
+		$quiz->load('questions.answers');
+
+		$correctAnswerCount = 0;
+		$points = 0;
+		foreach ($quiz->questions as $question) {
+			$correct = true;
+			foreach ($question->answers as $answer) {
+				if (in_array($answer->id, $answers) != $answer->is_correct) {
+					$correct = false;
+					break;
+				}
+			}
+			if ($correct) {
+				$correctAnswerCount++;
+				$points += $question->points;
+			}
+		}
+
+		Result::create([
+			'quiz_id' => $quiz->id,
+			'user_id' => auth()->id() ?? null,
+			'points'  => $points,
+			'time'    => $time,
+		]);
+
+		return response()->json([
+			'quiz_name'         => $quiz->title,
+			'quiz_level'        => $quiz->difficulty->name,
+			'correct_answers'   => $correctAnswerCount,
+			'incorrect_answers' => $quiz->questions->count() - $correctAnswerCount,
+			'time'              => $time,
+		]);
 	}
 }
